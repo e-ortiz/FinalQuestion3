b560017724bdd41d0b716a3ac6468f405fcaee05
Edwin Ortiz <eortiz@udel.edu>
2015-05-20 04:12:52 -0400

    welp

--- Question3Final/src/application/MortgageInterfaceController.java
+++ Question3Final/src/application/MortgageInterfaceController.java
@@ -10,65 +10,79 @@ import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class MortgageInterfaceController {
-	
-	// FXML Fields 
+
+	// FXML Fields
 	@FXML
 	private TextField totalGrossIncome;
 	@FXML
 	private TextField totalMonthlyDebt;
 	@FXML
-	private TextField mortgageInterestRate;	
-	
-	protected ObservableList<String> termOptions = FXCollections.observableArrayList("10", "15", "30");
+	private TextField mortgageInterestRate;
+
+	protected ObservableList<String> termOptions = FXCollections
+			.observableArrayList("10", "15", "30");
 	@FXML
-	protected ComboBox<ObservableList<String>> termBox = new ComboBox(termOptions);
-		
+	protected ComboBox<ObservableList<String>> termBox = new ComboBox(
+			termOptions);
+
 	@FXML
 	private TextField downPayment = new TextField("0");
-	
+
 	@FXML
 	protected Label housingPayment;
-	
+
 	@FXML
 	protected Label otherObligations;
-	
+
 	@FXML
 	protected Label maximumPayment;
-	
+
 	@FXML
 	protected Label financedMortgage;
-	
-	// Getters for TextFields -> Double
-	protected Double grossIncomeAmount = Double.valueOf(totalGrossIncome.getText());
-	
-	protected Double monthlyDebtAmount = Double.valueOf(totalMonthlyDebt.getText());
 
-	protected Double interestAmount = Double.valueOf(mortgageInterestRate.getText());
+	// Getters for TextFields -> Double
+	protected Double grossIncomeAmount = Double.valueOf(totalGrossIncome
+			.getText());
+
+	protected Double monthlyDebtAmount = Double.valueOf(totalMonthlyDebt
+			.getText());
+
+	protected Double interestAmount = Double.valueOf(mortgageInterestRate
+			.getText());
 
 	protected Double downPaymentAmount = Double.valueOf(downPayment.getText());
 
 	protected Double yearTerm = Double.valueOf(termBox.getPromptText());
-	
-	// Setters for Label Results -> String	
-	public void setHousingPayment(double amount) {
-	housingPayment.setText(String.valueOf(amount));
+
+	// Calculations
+	double HousingPaymentAmount = ((grossIncomeAmount / 12) * 0.18);
+
+	double OtherObligationsAmount = ((grossIncomeAmount / 12) * 0.36)
+			- monthlyDebtAmount;
+
+	private double MaximumPaymentAmount() {
+		if (HousingPaymentAmount >= OtherObligationsAmount)
+			return OtherObligationsAmount;
+		else
+			return HousingPaymentAmount;
 	}
 	
-	public void setOtherObligations(double amount) {
-	otherObligations.setText(String.valueOf(amount));
+	// Setters for Label Results -> String
+	public void setHousingPayment() {
+		housingPayment.setText(String.valueOf(HousingPaymentAmount));
 	}
-	
-	public void setPaymentAllowed(double amount) {
-	maximumPayment.setText(String.valueOf(amount));
+
+	public void setOtherObligations() {
+		otherObligations.setText(String.valueOf(OtherObligationsAmount));
 	}
-	
+
+	public void setPaymentAllowed() {
+		maximumPayment.setText(String.valueOf(MaximumPaymentAmount()));
+	}
+
 	public void setFinancedMortgage(double amount) {
-	financedMortgage.setText(String.valueOf(amount));
+		financedMortgage.setText(String.valueOf(amount));
 	}
-	
-	//Calculations
-	
-	
 	
 
 }
